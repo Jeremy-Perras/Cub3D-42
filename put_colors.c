63b9dc77dca5e73c8864c6254f54813c740ea0c8
JohnDowland <damirhaliti@yahoo.fr>
2022-05-02 12:12:22 +0200

    psu

--- put_colors.c
+++ put_colors.c
@@ -6,7 +6,7 @@
 /*   By: dhaliti <dhaliti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 11:37:18 by dhaliti           #+#    #+#             */
-/*   Updated: 2022/05/02 12:02:13 by dhaliti          ###   ########.fr       */
+/*   Updated: 2022/05/02 12:09:17 by dhaliti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,48 @@ void ft_put_colors(t_data *data)
 	char b[3] = {'0', '0'};
 	char g[3] = {'0', '0'};
 	char *floor;
-//	char *ceiling;
+	char *ceiling;
 	int m = 0;
 	int fd;
+	int fd2;
 
-		printf("%s\n", r);
 	ft_puthexa(data->f_r, &m, r);
 	m = 0;
-	printf("%s\n", r);
 	ft_puthexa(data->f_g, &m, g);
 	m = 0;
 	ft_puthexa(data->f_b, &m, b);
 	floor = ft_strjoin(r, ft_strjoin(g, b));
-	printf("%s\n", floor);
-	fd = open("./image/p.xpm", O_WRONLY);
-	if (fd < 0)
+	m = 0;
+	ft_puthexa(data->c_r, &m, r);
+	m = 0;
+	ft_puthexa(data->c_g, &m, g);
+	m = 0;
+	ft_puthexa(data->c_b, &m, b);
+	ceiling = ft_strjoin(r, ft_strjoin(g, b));
+	fd = open("./image/f.xpm", O_WRONLY);
+	fd2 = open("./image/c.xpm", O_WRONLY);
+	if (fd < 0 || fd2 < 0)
 		exit_error("floor error");
-	ft_putstr_fd("static char *_ee5d38949fd4231cdd0f319b3c19ae9VHAnEG3g9guP", fd);
-	ft_putstr_fd("SbUe[] = {\"50 50 1 1 \", \"  c #", fd);
+	ft_putstr_fd("static char *floor", fd);
+	ft_putstr_fd("SbUe[] = {\"5 5 1 1 \", \"  c #", fd);
 	ft_putstr_fd(floor, fd);
 	ft_putstr_fd(",", fd);
 	int i = 0;
-	while (i < 50)
+	while (i < 5)
 	{
 		ft_putstr_fd("\"                                                  \"", fd);
 		i++;
 	}
 	close(fd);
+	ft_putstr_fd("static char *ceiling", fd2);
+	ft_putstr_fd("SbUe[] = {\"5 5 1 1 \", \"  c #", fd2);
+	ft_putstr_fd(ceiling, fd2);
+	ft_putstr_fd(",", fd2);
+	i = 0;
+	while (i < 5)
+	{
+		ft_putstr_fd("\"                                                  \"", fd2);
+		i++;
+	}
+	close(fd2);
 }
